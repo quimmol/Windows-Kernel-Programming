d46ceecf3a5d0a6057a8760ed18f9634cf94153a
quim <expecles@protonmail.com>
2019-12-27 10:16:19 +0100

    switch -> break cases if -> bad condition

--- Chapter 1/getversion.cpp
+++ Chapter 1/getversion.cpp
@@ -27,35 +27,37 @@ DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 	KdPrint(("dwPlatformId: %d\n", osversion.dwPlatformId));
 	*/
 
-	if (!NT_SUCCESS(status))
+	if (NT_SUCCESS(status))
 	{
 		switch (osversion.dwMajorVersion)
 		{
-		case 10:
-			KdPrint(("Windows 10 BUILD %d\n", osversion.dwBuildNumber));
-		case 6:
-			if (osversion.dwMinorVersion == 3)
-			{
-				KdPrint(("Windows 8.1 BUILD %d\n", osversion.dwBuildNumber));
-			}
-			else if (osversion.dwMinorVersion == 2)
-			{
-				KdPrint(("Windows 8 BUILD %d\n", osversion.dwBuildNumber));
-			}
-			else if (osversion.dwMinorVersion == 1)
-			{
-				KdPrint(("Windows 7 BUILD %d\n", osversion.dwBuildNumber));
-			}
-			else
-			{
-				KdPrint(("Windows Vista BUILD %d\n", osversion.dwBuildNumber));
-			}
-		case 5: 
-			KdPrint(("Windows XP BUILD %d\n", osversion.dwBuildNumber));
-
-		default:
-			KdPrint(("Default case"));
-			break;
+			case 10:
+				KdPrint(("Windows 10 BUILD %d\n", osversion.dwBuildNumber));
+				break;
+			case 6:
+				if (osversion.dwMinorVersion == 3)
+				{
+					KdPrint(("Windows 8.1 BUILD %d\n", osversion.dwBuildNumber));
+				}
+				else if (osversion.dwMinorVersion == 2)
+				{
+					KdPrint(("Windows 8 BUILD %d\n", osversion.dwBuildNumber));
+				}
+				else if (osversion.dwMinorVersion == 1)
+				{
+					KdPrint(("Windows 7 BUILD %d\n", osversion.dwBuildNumber));
+				}
+				else
+				{
+					KdPrint(("Windows Vista BUILD %d\n", osversion.dwBuildNumber));
+				}
+				break;
+			case 5: 
+				KdPrint(("Windows XP BUILD %d\n", osversion.dwBuildNumber));
+				break;
+			default:
+				KdPrint(("Default case"));
+				break;
 		}
 	}
 	else
